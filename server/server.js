56fe164889ab36bf9b28689be80322d54ca6ac69
Praveen Singh Rawat <144586433+PraveenSinghrwt@users.noreply.github.com>
2024-02-15 15:57:16 +0530

    cors modified

--- server/server.js
+++ server/server.js
@@ -3,9 +3,22 @@ const app = express();
 const port = 5000;
 
 const cookieParser = require('cookie-parser');
-
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/BlogApp');
+
+
+const cors = require('cors');
+app.use(cors(
+    {
+        origin: ["https://blogger-server-nine.vercel.app/"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+))
+
+const DB = 'mongodb+srv://praveensinghrawat46:KsmwtchxT5g0jueA@cluster0.uewpwtk.mongodb.net/BlogApp?retryWrites=true&w=majority'
+mongoose.connect(DB).then(() => {
+    console.log('connection successful')
+}).catch((error) => console.log('no connection: ', error))
 
 app.use(cookieParser())
 app.use(express.json())
@@ -20,6 +33,7 @@ const createBlog = require('./Routes/createBlog');
 const fetchUserBlog = require('./Routes/fetchUserBlog');
 const fetchBlogs = require('./Routes/fetchBlog');
 const deleteBlog = require('./Routes/deleteBlog');
+const { db } = require('./models/User');
 
 
 app.use('/login', login);
@@ -29,6 +43,10 @@ app.use('/createBlog', createBlog);
 app.use('/fetchUserBlog', fetchUserBlog);
 app.use('/fetchBlogs', fetchBlogs);
 app.use('/deleteBlog', deleteBlog);
+
+app.get('/', (req, res) => {
+    res.send("hello");
+})
 
 
 app.listen(port, () => {
